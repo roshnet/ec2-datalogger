63cd459f256c1cdc1d47ca4ec223ab6eb2aa6c95
Roshan Sharma <reprogram46@gmail.com>
2021-07-26 21:29:12 +0530

    fix: Subscribe to topic after reconnect

--- actions.py
+++ actions.py
@@ -3,11 +3,6 @@ import json
 from models import Reading
 
 
-def on_connect(self, client, userdata, message):
-    """Callback when connected to the broker."""
-    pass
-
-
 def on_message(client, userdata, message):
     """Callback when message is received on any topic."""
     insert_payload(json.loads(message.payload.decode("utf-8")))
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from paho.mqtt.client import Client
 
-from actions import on_connect, on_message
+from actions import on_message
 
 load_dotenv()
 
@@ -14,13 +14,18 @@ BROKER_PASSWORD = os.environ.get("BROKER_PASSWORD")
 # Configure & establish connection to the broker
 mqtt_client = Client("Default Sensor")
 mqtt_client.username_pw_set(BROKER_USERNAME, BROKER_PASSWORD)
+
+
+def on_connect(self, client, userdata, message):
+    mqtt_client.subscribe("#")
+
+
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
 mqtt_client.connect(BROKER_IP)
-mqtt_client.subscribe("#")
 
 try:
     mqtt_client.loop_forever()
-except KeyboardInterrupt:
+except Exception as e:
     exit(0)
